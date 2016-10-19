2bcc9a5a8b5b07abe267de813d6ad5287ee9cf65
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2016-10-19 19:02:17 +0600

    Example added

---
+++ example/README.md
@@ -0,0 +1,13 @@
+# vue-dropzone-example
+
+> Example project for vue-dropzone component.
+
+## Build Setup
+
+``` bash
+# install dependencies.
+npm install
+
+# serve with hot reload at localhost:8080
+npm start
+```
---
+++ example/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <vue-dropzone :files.sync="files" v-ref:dropzone></vue-dropzone>
+        <hr/>
+        <a @click="getFiles">Get dropped files</a>
+        <ul class="file-list">
+            <li v-for="file in files">
+                <img :src="file.thumbnail" alt="" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Dropzone from 'vue-dropzone';
+
+export default {
+    name: 'App',
+    components: { Dropzone },
+    data () {
+        return {
+            files: []
+        };
+    }
+};
+
+</script>
